f0278f4403f95ba3133061c7e6a70d3b3892be80
IraLysenko <arch_ira@ukr.net>
2023-02-16 18:06:06 +0100

    chore: show more authors

--- zen-quotes-app/src/app/authors/authors.component.ts
+++ zen-quotes-app/src/app/authors/authors.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Author} from "../../types/author.type";
 
 @Component({
@@ -7,6 +7,22 @@ import {Author} from "../../types/author.type";
   styleUrls: ['./authors.component.scss']
 })
 
-export class AuthorsComponent {
+export class AuthorsComponent implements OnInit{
   @Input() authors: Author[] = [];
+  authorsQuantity: number = 33;
+
+  constructor() {
+    console.log('onInit'+this.authors)
+  }
+
+  ngOnInit(): void {
+    console.log('onInit'+this.authors.length)
+  }
+
+  showMoreAuthors() {
+    if(this.authors.length > this.authorsQuantity) {
+      this.authorsQuantity += this.authorsQuantity;
+    }
+  }
+
 }

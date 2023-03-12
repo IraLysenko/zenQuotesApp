4fa36b7ccf5a025616d4d6af620f9acfa3f51a7e
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2023-03-12 15:09:34 +0100

    feat(home): add helpers

---
+++ zen-quotes-app/src/app/home/helpers.ts
@@ -0,0 +1,8 @@
+// @ts-ignore
+// @Todo
+//  @Ira: when we run `ng test`, the tests in helpers.spect.ts fail.
+//  Can you implement this function so that the tests succeed?
+//  Thanks and best of luck! 🍀
+export function makeAuthorsUnique(authors) {
+  return authors;
+}

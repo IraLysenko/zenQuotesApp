91d6be80eb492ef1c159de98267dcd2e61d594d6
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2023-03-12 14:55:53 +0100

    fix(*): fix failing tests

--- zen-quotes-app/src/app/quotes.service.spec.ts
+++ zen-quotes-app/src/app/quotes.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { QuotesService } from './quotes.service';
@@ -6,7 +7,9 @@ describe('QuotesService', () => {
   let service: QuotesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(QuotesService);
   });
 

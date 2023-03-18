4069e6df8e3428c959395e4e87c4718190630bd0
IraLysenko <arch_ira@ukr.net>
2023-03-18 23:38:40 +0100

    refactor: added functions to helpers

--- zen-quotes-app/src/app/home/helpers.ts
+++ zen-quotes-app/src/app/home/helpers.ts
@@ -1,8 +1,32 @@
-// @ts-ignore
 // @Todo
 //  @Ira: when we run `ng test`, the tests in helpers.spect.ts fail.
 //  Can you implement this function so that the tests succeed?
 //  Thanks and best of luck! 🍀
-export function makeAuthorsUnique(authors) {
-  return authors;
+import * as _ from 'lodash';
+
+import { Author } from '../../types/author.type';
+import { Quote } from '../../types/quote.type';
+
+export function mapAuthorsFromQuotes(quotes: Quote[] | []): Author[] {
+  return quotes
+    ? quotes.map((quote) => {
+        return { name: quote.author };
+      })
+    : [];
+}
+
+export function makeAuthorsUnique(authors: Author[] | undefined): Author[] | [] {
+  return authors ? [...new Set(authors)] : [];
+}
+
+export function sortAuthors(authors: Author[] | undefined): Author[] | [] {
+  return authors ? _.orderBy(authors, 'name', ['asc']) : [];
+}
+
+export function getAuthors(quotes: Quote[] | undefined): Author[] | [] {
+  return quotes ? sortAuthors(makeAuthorsUnique(mapAuthorsFromQuotes(quotes))) : [];
+}
+
+export function generateRandomQuote(quotes: Quote[] | undefined): Quote {
+  return quotes ? quotes[Math.floor(Math.random() * quotes.length)] : { author: '', text: '' };
 }
